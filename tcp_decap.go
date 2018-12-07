e70c965229bacf8bd64619012c9f4bd664352522
ycouble <4104153+ycouble@users.noreply.github.com>
2018-12-07 22:46:46 +0100

    Almost working ! Still need to fix the bad types

--- tcp_decap.go
+++ tcp_decap.go
@@ -4,9 +4,9 @@ import (
     "flag"
     "fmt"
     "log"
-    "io"
-//    "github.com/google/gopacket"
-//    "github.com/google/gopacket/layers"
+//    "io"
+    "github.com/google/gopacket"
+    "github.com/google/gopacket/layers"
     "github.com/google/gopacket/pcap"
 //    "github.com/google/gopacket/tcpassembly"
 )
@@ -18,7 +18,8 @@ var verbose = flag.Bool("v", false, "Display intermediate layers informations")
 func main() {
     flag.Parse()
     if *fname == "" {
-	fmt.Printf("Need an input file name: Format : tcp_decap -r <filename.pcap>\n")
+	fmt.Printf("Need an input file name: Format : ",
+		    "tcp_decap -r <filename.pcap>\n")
 	return
     }
 
@@ -31,22 +32,65 @@ func main() {
 
     pkt := 0
 
-    for {
-	data, ci, err := handleRead.ReadPacketData()
-	fmt.Println(data)
-	switch {
-	    case err == io.EOF:
-		fmt.Printf("\nFinished the PCAP\n")
-		return
-	    case err != nil:
-		log.Printf("Failed to read packet %d: %s", pkt, err)
-		return
-	    default:
-		fmt.Println(data, ci)
-		pkt++
+    // First, we need to create the packet decoder for Ethernet
+    // What if not Ethernet ???
+    var decoder gopacket.Decoder
+    decoder, ok := gopacket.DecodersByLayerName["Ethernet"]
+    if !ok {
+	log.Fatalln("Bad decoder name ??!!")
+    }
+    // Generate the source from the pcap handle and the decoder
+    // Reminder: a source is a struct composed of a packet data source 
+    // (coming from online/file) and a decoder (LayerDecoder). It also has
+    // a chan c and DecodeOptions
+    source	    := gopacket.NewPacketSource(handleRead, decoder)
+    source.Lazy	    = false
+    source.NoCopy   = true
+    source.DecodeStreamsAsDatagrams = true
 
+    for packet := range source.Packets(){
+	// PacketSource.Packets() returns a chan of max 1000 packets 
+	fmt.Printf("Packet %d - %d Layers\n", pkt, len(packet.Layers()))
+	for i, layer := range packet.Layers(){
+	    fmt.Printf("-- Layer %d: %T\n", i, layer)
+	    // Display layer information, for the main layer types
+	    switch layer.LayerType() {
+		case layers.LayerTypeEthernet:
+		    PrintEthInfo(layer)
+		case layers.LayerTypeIPv4:
+		    PrintIPv4Info(layer)
+		case layers.LayerTypeTCP:
+		    PrintTCPInfo(layer)
+		case layers.LayerTypeUDP:
+		    PrintUDPInfo(layer)
+		default:
+		    fmt.Printf("Layer %s not implemented yet",
+				layer.LayerType())
+	    }
 	}
+
+	// Packet present in data
+	pkt++
+	// Test for -f option
+	if *first { return }
     }
 }
 
+func PrintEthInfo(layer gopacket.Layer) {
+    fmt.Printf("MAC SRC: %s\tMAC DST: %s", layer.SrcMAC, layer.DstMAC)
+    fmt.Printf("EtherType: %s", layer.EthernetType)
+}
+
+func PrintIPv4Info(layer layers.LayerTypeIPv4) {
+    fmt.Printf("@IP SRC: %s\t@IP DST: %s", layer.SrcIP, layer.DstIP)
+    fmt.Printf("Protocol: %s", layer.Protocol)
+}
+
+func PrintTCPInfo(layer layers.LayerTypeTCP) {
+    fmt.Println("Not Implemented Yet")
+}
+
+func PrintUDPInfo(layer layers.LayerTypeUDP) {
+    fmt.Println("Not Implemented Yet")
+}
 
